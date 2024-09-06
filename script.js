73225fb39feef9d251bff4aea64b566ac27864c4
Sonic Shouko <pedro.henrique.con@gmail.com>
2024-09-06 20:56:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Bem-vindo ao Pérola Azul!');
+});
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('myButton');
+    
+    button.addEventListener('click', function() {
+        alert('Botão clicado!');
+    });
+});
+
+
